312b4a221588e016c2b54750ce5b525c71437a44
wenzx1993 <1006380802@qq.com>
2019-08-20 23:18:26 +0800

    eureka-demo-one

---
+++ SpringCloudEurekaDemo/eureka-client-one/src/main/java/com/springcloud/eurekaclientone/EurekaClientOneApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.eurekaclientone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientOneApplication.class, args);
+    }
+
+}
---
+++ SpringCloudEurekaDemo/src/main/java/com/springcloud/demo/SpringCloudEurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaDemoApplication.class, args);
+    }
+
+}
