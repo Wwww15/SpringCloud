8d99bcd9c751bc2a60cc58b0fa2faa05077158c3
Wenzx1993 <1006380802@qq.com>
2019-11-22 09:39:18 +0800

    Spring cloud sleuth exercise

--- SpringCloudSleuth/user-client/src/main/java/com/springcloud/userclient/UserClientApplication.java
+++ SpringCloudSleuth/user-client/src/main/java/com/springcloud/userclient/UserClientApplication.java
@@ -2,8 +2,10 @@ package com.springcloud.userclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserClientApplication {
 
     public static void main(String[] args) {
