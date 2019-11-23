99e51d811b80d04de70bf76531352aa351362f4d
Wenzx1993 <1006380802@qq.com>
2019-11-23 15:40:27 +0800

    spring cloud security test

---
+++ SpringBootSecurity/src/main/java/com/springboot/security/config/SpringSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.springboot.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig  extends WebSecurityConfigurerAdapter {
+
+
+
+}
