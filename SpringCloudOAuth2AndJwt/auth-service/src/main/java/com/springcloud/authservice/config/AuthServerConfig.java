4172898504cd42034d6160d3a35be0d687fdab92
Wenzx1993 <1006380802@qq.com>
2019-12-20 18:21:11 +0800

    SpringCloudOauth2 advance:

--- SpringCloudOAuth2AndJwt/auth-service/src/main/java/com/springcloud/authservice/config/AuthServerConfig.java
+++ SpringCloudOAuth2AndJwt/auth-service/src/main/java/com/springcloud/authservice/config/AuthServerConfig.java
@@ -1,20 +1,27 @@
 package com.springcloud.authservice.config;
 
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    public AuthenticationManager authenticationManager;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -29,13 +36,25 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(new JwtTokenStore())
+        endpoints
+                //存储策略
+                .tokenStore(new JwtTokenStore(jwtTokenEnhancer()))
+                //增强策略
+                .tokenEnhancer(jwtTokenEnhancer())
+                //开启密码授权验证
+                .authenticationManager(authenticationManager);
     }
 
+
+
+    //jwt身份转换器，用于将封装在jwt里面的信息解析或者转换
     public JwtAccessTokenConverter jwtTokenEnhancer()
     {
-        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory();
-
+        //RSA非对称加密密钥工厂，参数一为密钥地址，参数二为打开密钥的密码
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("auth-jwt.jks"),"jwtsecret12345".toCharArray());
+        JwtAccessTokenConverter converter =  new JwtAccessTokenConverter();
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("auth-jwt"));
+        return converter;
     }
 
 }
